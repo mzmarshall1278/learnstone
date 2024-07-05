52569515ebc28f0a1e992f78b10d1d4668fbc7e9
mzmarshall1278 <mzmarshall1278@gmail.com>
2024-07-05 21:29:06 +0100

    more assets

---
+++ pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="">
+    <div class="noise-bg">
+      <nav-bar />
+      <!-- Hero -->
+      <div class="my-11 lg:flex justify-between w-5/6 mx-auto">
+        <div class="w-full lg:w-[55%] text-beige-1 my-auto">
+          <h1 class="large-font">
+            Discover Your Path to Success with Learnstone
+          </h1>
+          <h5 class="text-beige tracking-wider text-2xl md:text-3xl my-12">
+            Find the Perfect Postgraduate Course for Your Future
+          </h5>
+          <button class="btn-primary">Start Exploring</button>
+        </div>
+        <img src="/hero.png" class="w-full lg:w-[45%] object-contain" />
+      </div>
+    </div>
+
+    <div class="bg-beige-1 py-10">
+      <div class="w-5/6 lg:w-11/12  mx-auto lg:flex gap-4 xl:gap-16 relative">
+        <div class="relative flex justify-end w-[200px] ml-auto md:ml-0 md:w-1/2 lg:w-[480px] xl:w-[580px] lg:ml-16">
+          <img
+            src="/dots.svg"
+            alt="dots"
+            class="w-full md:w-1/2 xl:w-4/5 absolute lg:relative"
+          />
+          <img
+            src="/ellipse3.svg"
+            alt="ellipse"
+            class="w-full hidden lg:block md:w-1/2 xl:w-4/5 absolute lg:relative"
+          />
+        </div>
+
+        <div
+          class="my-auto text-darkGreen md:w-[512px] lg:w-1/3 xl:w-2/5 py-24 lg:py-1"
+        >
+          <h3 class="large-font mb-12">What is Learnstone?</h3>
+          <p class="">
+            Learnstone is an upcoming online platform designed to empower
+            students and young professionals in achieving their career
+            aspirations. Our mission is to streamline the process of identifying
+            learning opportunities by harnessing the power of data and
+            artificial intelligence.
+          </p>
+        </div>
+      </div>
+
+      <!-- <div
+        class="w-2/3 mt-6 mx-auto blueNoise-bg p-11 rounded-2xl justify-end xl:flex relative"
+      >
+        <div class="text-beige-1 w-full xl:w-[45%] 2xl:w-[512px] my-auto">
+          <h1 class="large-font mb-11">How Does Learnstone Work?</h1>
+          <p class="">
+            We meticulously gather and organize a vast amount of data on various
+            learning opportunities worldwide. Through advanced algorithms, we
+            structure and analyze this data to provide meaningful insights and
+            guidance to our users.
+          </p>
+        </div>
+        <img
+          src="/business.png"
+          alt=""
+          class="w-full xl:w-[45%] 2xl:w-[535px] pt-8 xl:pt-1 object-contain"
+        />
+      </div> -->
+
+      <!-- <div class="flex mt-24 w-[75%] mx-auto justify-between">
+        <div class="flex relative"></div>
+
+        <div class="w-[700px]">
+          <div class="w-5/6">
+            <h1 class="large-font text-darkGreen mb-12">
+              Our Comprehensive Platform
+            </h1>
+            <p class="text-green text-xl font-normal">
+              Learnstone serves as a comprehensive guide, enabling users to make
+              informed decisions about their educational paths and career
+              progression. We believe that by providing clarity and direction,
+              we can significantly contribute to the success of students and
+              young professionals worldwide.
+            </p>
+          </div>
+        </div>
+      </div> -->
+    
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
