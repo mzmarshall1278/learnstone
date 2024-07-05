33aaa90550ea305ed76a372735aa11e0a1fbd396
mzmarshall1278 <mzmarshall1278@gmail.com>
2024-07-05 21:29:55 +0100

    initialized navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="pt-14 flex justify-between w-5/6 mx-auto py-4">
+        <img src="/logo.svg" class="my-auto" alt="logo" />
+
+        <div class="flex gap-2">
+            <div class="flex  bg-beige-1 rounded-xl px-8 py-1">
+                <NuxtLink to="/" class="my-auto px-2">Details</NuxtLink>
+                <NuxtLink to="/" class="my-auto px-2">Pricing</NuxtLink>
+                <NuxtLink to="/" class="my-auto px-2">Team</NuxtLink>
+            </div>
+            <button class="btn-primary">Get in Touch</button>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
